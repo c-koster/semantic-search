09d91146c55d67e4b1961bd99151e89df90cfd9a
c-koster <52757828+c-koster@users.noreply.github.com>
2022-03-15 14:48:42 -0400

    reddit data too

--- download_from_GCP.py
+++ download_from_GCP.py
@@ -6,15 +6,15 @@ from google.oauth2 import service_account
 
 path_base = os.getcwd()
 
-
-
 key_path = path_base + '/nwo-sample-5f8915fdc5ec.json'
+
 credentials = service_account.Credentials.from_service_account_file(
     key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
 )
-
 client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
 
+
+'''
 # first, get all my tweets read into a parquet
 SMALL_TWEETS = """
 SELECT tweet, created_at FROM `nwo-sample.graph.tweets` LIMIT 10000;
@@ -26,3 +26,16 @@ df = rows.to_dataframe(progress_bar_type='tqdm')
 print(df.head())
 
 df.to_parquet(path_base + "/df_tweets.parquet")
+'''
+# second do the exact same thing with reddit
+SMALL_REDDIT = """
+SELECT body, created_utc FROM `nwo-sample.graph.reddit` LIMIT 10000;
+"""
+
+query_job = client.query(SMALL_REDDIT)
+rows = query_job.result()
+df = rows.to_dataframe(progress_bar_type='tqdm')
+
+print(df.head())
+
+df.to_parquet(path_base + "/df_reddit.parquet")
