bc94c4d2e36ccc71ce958d42fb09380923c50b34
c-koster <52757828+c-koster@users.noreply.github.com>
2022-03-17 11:26:01 -0400

    implemented with .py files. also added the notebook

--- make_gensim_models.py
+++ make_gensim_models.py
@@ -16,7 +16,36 @@ My process for creating these models is outlined briefly below (also see the rea
     - (later we will boost the ones that score highly on both)
 
 """
+
+# imports
 import os
+from typing import List
+
+# tagging and data storage
+import pandas as pd
+from nltk.tokenize import TweetTokenizer
+from nltk.tag import pos_tag
+from spacy.lang.en.stop_words import STOP_WORDS
+import emoji
+
+from gensim.models import Word2Vec
+
+
+from tqdm import tqdm
+tqdm.pandas() # .apply progress bars
+
+
+# models
+from gensim.models.phrases import Phrases, Phraser
+from gensim.models import Word2Vec
+
+
+# consts for tagging
+STOP_EMOJI = set(emoji.UNICODE_EMOJI.keys())
+STOP_WORDS_STOP_EMOJIS = STOP_WORDS.union(STOP_EMOJI)
+GOOD_TAGS = {
+    'NN','NNP','NNPS','NNS'#,'VBD','VB','VBG','VBN','VBP','VBZ'
+}
 
 path_base = os.getcwd()
 
@@ -24,7 +53,94 @@ path_base = os.getcwd()
 assert os.path.exists(path_base + "/df_tweets.parquet")
 assert os.path.exists(path_base + "/df_reddit.parquet")
 
+# build dfs and make sure they are in the same time format
+df_tweets = pd.read_parquet('/Users/cultonkoster/Desktop/homework/nwo-case-studies/nlp/df_tweets.parquet')
+df_reddit = pd.read_parquet('/Users/cultonkoster/Desktop/homework/nwo-case-studies/nlp/df_reddit.parquet')
+df_tweets["type"] = 1
+df_reddit["type"] = 2
 
 
-from typing import List
-# build dfs
+df_tweets['created_at'] = pd.to_datetime(df_tweets['created_at'])
+
+
+
+to_int = lambda timedata: timedata.timestamp()
+df_tweets['created_utc'] = df_tweets.pop('created_at').apply(to_int).astype('int64')
+
+
+tweet_tokenizer = TweetTokenizer()
+
+def preprocess_tweets(text: str) -> List[str]:
+    """
+    Pipeline for processing text. The trick here is to get all useful words into a
+    sequence/list and then run gensim's phraser on the sentences. This is likely to be
+    more useful as it does not rely on correct than pasting together proper NNP tags.
+
+    Parameters:
+        A string containing twitter text
+
+    Returns a list of words.
+    """
+    tokens = tweet_tokenizer.tokenize(text)
+    tags = pos_tag(tokens)
+
+    words_out: List[str] = []
+
+    for word,tag in tags:
+
+        if tag not in GOOD_TAGS or word[:4] == "http" or len(word) < 3 or word in STOP_WORDS_STOP_EMOJIS:
+            continue
+        else:
+            # we want to join contiguous proper nouns
+            words_out.append(word.lower())
+
+    return words_out
+
+
+# run the above preprocessing function
+print("pre-processing Twitter text")
+df_tweets["text_preprocessed"] = df_tweets["tweet"].progress_apply(preprocess_tweets)
+
+print("pre-processing Reddit text")
+df_reddit["text_preprocessed"] = df_reddit["body"].progress_apply(preprocess_tweets)
+
+
+# now add the two together before we extract phrases.. do this by calling their columns
+# the same thing (text and created at) and stacking one on top of the other
+df_all = pd.concat([df_tweets.drop('tweet',axis=1), df_reddit.drop('body',axis=1)])
+
+
+
+# the last preprocessing thing I want to do before grouping:
+# i've noticed in earlier iterations that hashtags and user tags (#,@) tend to get glued together,
+# Let's remove these and then add them back in once phrasing is done.
+hash_or_user_tag = lambda w: (w[0] == '#' or w[0] == '@')
+
+filter_out  = lambda words: [word for word in words if not hash_or_user_tag(word)]
+filter_only = lambda words: [word for word in words if hash_or_user_tag(word)]
+
+df_all['words_no_@#'] = df_all['text_preprocessed'].apply(filter_out)
+df_all['words_@#'] = df_all['text_preprocessed'].apply(filter_only)
+
+
+phrases = Phrases(df_all['words_no_@#'], min_count=4, threshold=2)
+phraser = Phraser(phrases)
+
+def sentence_to_bi_grams(phrases_model, sentence: List[str]) -> List[str]:
+    return phrases_model[sentence]
+
+apply_phrases = lambda sentence: sentence_to_bi_grams(phraser,sentence)
+
+df_all['text_with_phrases'] = df_all['words_no_@#'].apply(apply_phrases) + df_all['words_@#']
+
+print("some examples of phrases for your viewing pleasure while the w2v models train")
+for phrase, score in sorted(phrases.find_phrases(df_all['text_preprocessed']).items(), key=lambda tup: tup[1],reverse=True)[:20]:
+    print(phrase, score)
+
+
+model = Word2Vec(sentences=df_all["text_with_phrases"], vector_size=100, window=10, min_count=2, workers=4)
+model.wv.save('long_term_vectors.kv')
+
+# created_utc of 1610496000 is Wednesday, January 13, 2021 12:00:00 AM (GMT)
+model_st = Word2Vec(sentences=df_all[df_all.created_utc > 1610496000]["text_with_phrases"])
+model_st.wv.save('short_term_vectors.kv')
---
+++ similar_trends_method.py
@@ -0,0 +1,44 @@
+from gensim.models import KeyedVectors
+from collections import defaultdict
+
+
+model    = KeyedVectors.load('long_term_vectors.kv')
+model_st = KeyedVectors.load('short_term_vectors.kv')
+
+
+def w2v_based_top_trends(qt: str, k: int = 20, k_inner: int = 200):
+    # ensure the query term has been , and
+    assert k < k_inner
+    parsed_qt = qt.lower().strip().replace(" ","_")
+
+    # do a full join on these lists. i don't know a way to do in a fancy or less-verbose way in python so here:
+    try:
+        long_term_trends  = model.most_similar(parsed_qt, topn=k)
+        short_term_trends = model_st.most_similar(parsed_qt, topn=k)
+    except KeyError:
+        return [("did not find that trend",0)]
+
+    st_scores = defaultdict(int)
+    lt_scores = defaultdict(int)
+
+    for word, relevance in short_term_trends:
+        st_scores[word] = relevance
+
+    for word, relevance in long_term_trends:
+        lt_scores[word] = relevance
+
+
+    # do a full join on these lists
+    all_words = set([w for w,_ in long_term_trends] + [w for w,_ in short_term_trends])
+
+    joined_list = [(word,st_scores[word],lt_scores[word]) for word in all_words]
+
+    return [w for w, st_score, lt_score in sorted(joined_list, key=lambda tup: tup[2], reverse=True)[:k]]
+
+K = 5
+test_terms = ["augmented_reality", "iphone", "elon musk", "bike", "joe biden",
+"kombucha", "bitcoin", "student loans","gold","pepsi","virtual reality","@realdonaldtrump"]
+
+for i in test_terms:
+    trends = w2v_based_top_trends(i,K)
+    print( "Query Term: {}\nResults: {}\n".format(i, trends) )
